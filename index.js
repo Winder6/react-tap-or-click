cbf070d55417344f9035b1c4d23f8bf00b58d6f1
Ivan Zhou <ivan.zhou@makeblock.com>
2018-07-20 17:02:41 +0800

    fix move

--- index.js
+++ index.js
@@ -1,77 +1,148 @@
 'use strict'
 
-var isFunction = (function() {
-    function typeOfFn(fn) {
-        return typeof fn === 'function'
-    }
+// http://ariatemplates.com/blog/2014/05/ghost-clicks-in-mobile-browsers/
+var bustableClicks = []
 
-    function objectFn(fn) {
-        return Object.prototype.toString.call(fn) === '[object Function]'
-    }
+function addBuster(touchEvent) {
+  if (!touchEvent || !('changedTouches' in touchEvent)) {
+    throw new Error('No event or no changedTouches in event object')
+  }
 
-    // typeof is fastest way to check if a function but older IEs don't support it for that and Chrome had a bug
-    if (typeof typeOfFn === 'function' && typeof /./ !== 'function') {
-        return typeOfFn
-    }
-
-    return objectFn
-})()
-
-function getCallbackHandlers(callback) {
-        var state = {}
-
-        var handler = {
-            callback: callback,
-            touchStart: function(event) {
-                if (event.defaultPrevented) {
-                    return
-                }
-
-                clearTimeout(state.touchTimeout)
-                state.touchClick = true
-                callback(event)
-            },
-            touchEnd: function(event) {
-                if (state.touchClick) {
-                    state.touchTimeout = setTimeout(function() {
-                        state.touchClick = false
-                        state.touchTimeout = null
-                    }, 50)
-                }
-            },
-            click: function(event) {
-                if (event.defaultPrevented || state.touchClick) {
-                    return
-                }
-                callback(event)
-            }
-        }
-
-    return handler
+  bustableClicks.push({
+    element: touchEvent.target,
+    x: touchEvent.changedTouches[0].screenX,
+    y: touchEvent.changedTouches[0].screenY
+  })
 }
 
-// event handlers are unnecessary server side
-if (typeof window === 'undefined') {
-    module.exports = function(callback, props) {
-        if (props == null) {
-            props = {}
-        }
-        return props
+function buster(event) {
+  if (bustableClicks.length === 0) {
+    return
+  }
+
+  var x = ~~event.screenX
+  var y = ~~event.screenY
+  var i
+  var b
+
+  var xMin = x - 50
+  var xMax = x + 50
+  var yMin = y - 50
+  var yMax = y + 50
+
+  for (i = 0; i < bustableClicks.length; i++) {
+    b = bustableClicks[i]
+    if (b.x > xMin && b.x < xMax && b.y > yMin && b.y < yMax) {
+      event.preventDefault()
+      event.stopImmediatePropagation()
+      break
     }
-} else {
-    module.exports = function tapOrClick(callback, props) {
-        if (props == null) {
-            props = {}
-        } else if (typeof props !== 'object') {
-            throw new Error('Optional second argument to tapOrClick must be a mutable object')
-        }
+  }
+}
 
-        var handlers = getCallbackHandlers(callback)
-
-        props.onTouchStart = handlers.touchStart
-        props.onTouchEnd = handlers.touchEnd
-        props.onClick = handlers.click
-
-        return props
+function removeBusterByElement(element) {
+  for (var i = bustableClicks.length - 1; i >= 0; i--) {
+    if (bustableClicks[i].element === element) {
+      bustableClicks.splice(i, 1)
     }
+  }
+}
+
+if (typeof document !== 'undefined' && 'addEventListener' in document) {
+  document.addEventListener('click', buster, true)
+}
+
+// handler cache to
+// 1) avoid unnecessary duplicates for same callback, and
+// 2) to warn in case of possible issues in renders
+var cache = []
+
+function endTouch(state) {
+  state.touches = 0
+  state.touchTimeout = null
+  removeBusterByElement(this)
+}
+
+function createHandlers(callback) {
+
+  var state = { touches: 0 }
+
+  var handler = {
+    callback: callback,
+    touchStart: function(event) {
+      if (event.defaultPrevented) {
+        return
+      }
+      clearTimeout(state.touchTimeout)
+      state.touches++
+      state.touchTimeout = setTimeout(endTouch.bind(event.target, state), 250)
+      state.x = event.changedTouches[0].screenX
+      state.y = event.changedTouches[0].screenY
+    },
+    touchEnd: function(event) {
+      if (event.defaultPrevented || state.touches !== 1) {
+        return
+      }
+
+      var deltaX = state.x - event.changedTouches[0].screenX
+      var deltaY = state.y - event.changedTouches[0].screenY
+
+      if (Math.abs(deltaX) < 5 && Math.abs(deltaY) < 5) {
+        clearTimeout(state.touchTimeout)
+        addBuster(event)
+        event.preventDefault()
+        callback(event)
+        state.touchTimeout = setTimeout(endTouch.bind(event.target, state), 425)
+      }
+    },
+    click: function(event) {
+      if (event.defaultPrevented || state.touches > 0) {
+        return
+      }
+      callback(event)
+    }
+  }
+
+  return handler
+}
+
+function getHandlers(callback) {
+  return createHandlers(callback)
+}
+
+var isFunction = (function() {
+  function typeOfFn(fn) {
+    return typeof fn === 'function'
+  }
+
+  function objectFn(fn) {
+    return Object.prototype.toString.call(fn) === '[object Function]'
+  }
+
+  // typeof is fastest way to check if a function but older IEs don't support it for functions and Chrome had a bug
+  if (typeof typeOfFn === 'function' && typeof /./ !== 'function') {
+    return typeOfFn
+  }
+
+  return objectFn
+})()
+
+module.exports = function tapOrClick(callback, props) {
+  if (!isFunction(callback)) {
+    throw new Error('First argument to tapOrClick must be a callback function')
+  }
+
+  if (props == null) {
+    props = {}
+  } else if (typeof props !== 'object') {
+    throw new Error('Optional second argument to tapOrClick must be a mutable object')
+  }
+
+  var handlers = getHandlers(callback)
+
+  props.onTouchStart = handlers.touchStart
+  props.onTouchEnd = handlers.touchEnd
+  props.onClick = handlers.click
+
+  return props
 }
